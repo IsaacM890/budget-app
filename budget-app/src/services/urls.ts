a10d80800b9c376a76e3cd83cb4d9690febb4987
IsaacM <Itshak890@gmail.com>
2021-03-31 15:50:02 +0300

    adding urls for api calls

---
+++ budget-app/src/services/urls.ts
@@ -0,0 +1,6 @@
+export const baseURL = `https://budget-application-services-isaacm890.vercel.app/api/`;
+
+export const endPoints = {
+  user: `${baseURL}users`,
+  transaction: `${baseURL}transactions`,
+};
