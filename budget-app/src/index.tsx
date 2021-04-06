0df841db6b8f8df3deee774219266eb1d328b47a
IsaacM <Itshak890@gmail.com>
2021-04-06 21:18:55 +0300

    changing text decoration

--- budget-app/src/index.tsx
+++ budget-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import theme from './style/theme/theme';
 
